7372c4edcf3045dd309bd3015d1cdd1c4bcb7446
gonchigars <gonchigars@gmail.com>
2024-03-28 08:29:56 +0530

    first commit

---
+++ basics/src/main/java/com/academix/App.java
@@ -0,0 +1,13 @@
+package com.academix;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ basics/target/classes/com/academix/Readme.txt
@@ -0,0 +1,3 @@
+academix git:(main) ✗ cd "/Users/shashank/Academix/java_1/basics/src/main/java/" && javac com
+/academix/App.java && java com.academix.App
+
