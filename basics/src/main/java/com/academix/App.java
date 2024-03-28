7eeffd0311a00b67f76ebb61d3647fab17b07a77
gonchigars <gonchigars@gmail.com>
2024-03-28 08:46:18 +0530

    second commit

--- basics/src/main/java/com/academix/App.java
+++ basics/src/main/java/com/academix/App.java
@@ -1,13 +1,33 @@
 package com.academix;
 
-/**
- * Hello world!
- *
+import java.util.Scanner;
+
+/*
+ * Class App will take inputs from user and print the sum
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    /*
+     * Class App will take inputs from user and print the sum
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        int sum = calculateSum(num1, num2);
+        System.out.println("Sum: " + sum);
+
+        scanner.close();
+    }
+
+    /*
+     * Calculates the sum of two numbers
+     */
+    public static int calculateSum(int num1, int num2) {
+        return num1 + num2;
     }
 }
